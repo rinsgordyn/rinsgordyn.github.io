4e532d2129d9aca88b5b265a90295b3dfb3a6391
rinsgordyn <rinsgordyn@gmail.com>
2025-05-04 10:03:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function beli(namaProduk) {
-  const nomorWA = "6281234567890"; // Ganti dengan nomor WhatsApp Anda (format internasional, tanpa +)
+  const nomorWA = "6281384990679"; // Ganti dengan nomor WhatsApp Anda
   const pesan = `Halo, saya ingin membeli produk ${namaProduk}. Apakah masih tersedia?`;
   const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
   window.open(url, '_blank');
-  return false; // Mencegah link default
+  return false;
 }
